ed0471adcad8d9929a5ff8d5299755f691edbddb
Michal Hornicky <semtexzv@gmail.com>
2023-08-09 14:08:29 +0200

    feat: Prepare for release

---
+++ scripts/publish.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+(cd protokit_binformat && cargo publish)
+(cd protokit_textformat && cargo publish)
+(cd protokit_derive && cargo publish)
+(cd protokit_desc && cargo publish)
+(cd protokit_proto && cargo publish)
+(cd protokit_build && cargo publish)
+(cd protokit && cargo publish)
--- tools/conformance/src/gen/protobuf_test_messages/mod.rs
+++ tools/conformance/src/gen/protobuf_test_messages/mod.rs
@@ -1,5 +1,5 @@
-pub mod proto2;
 pub mod proto3;
+pub mod proto2;
 pub fn register_types(registry: &mut ::protokit::textformat::reflect::Registry) {
     proto3::register_types(registry);
     proto2::register_types(registry);
